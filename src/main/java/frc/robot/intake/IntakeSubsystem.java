684775157975477380eb7e8c0816663cd68a39a2
Jonah Snider <jonah@jonahsnider.com>
2025-10-28 18:18:17 -0700

    Format
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.intake;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANdi;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.S2StateValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -17,18 +16,13 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   /** Runs the intake until a coral is detected. */
-  public void intake() {
-  }
+  public void intake() {}
 
   /** Runs the intake in reverse to score a coral. */
-  public void score() {
-
-  }
+  public void score() {}
 
   /** Stops the intake. */
-  public void stop() {
-
-  }
+  public void stop() {}
 
   /** Returns whether the intake is holding a coral. */
   private boolean intakeHasCoral() {
